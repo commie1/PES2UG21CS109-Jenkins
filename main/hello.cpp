0a2a83180972ab1ee4709d75f574a4b0605fcade
Ayushmaan Singh Nikumbh <91002904+commie1@users.noreply.github.com>
2024-03-07 07:41:11 +0530

    webhook

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins i am editing this file\n";
+  cout << "Hello, Jenkins \n";
   cout << "I have successfully built and run\n";
+  cout << "Webhook successfully enabled\n";
   return 0;
 }
